103d1f6ec4beb98ebc39da142d12734e0099fb0b
danie96 <danieleappetito@gmail.com>
2020-06-02 12:38:22 +0200

    added ysceneproc.py e aggiunto le funzioni necessarie in extension

--- apps/ysceneproc/ysceneproc.py
+++ apps/ysceneproc/ysceneproc.py
@@ -2,7 +2,26 @@ import py_commonio as cli
 import py_sceneio as sio
 import py_math 
 import py_pathtrace as ptr
+import py_filesystem as fs
 import sys
+
+def make_preset(scene, type, error):
+  if(type == "cornellbox"):
+    sio.make_cornellbox(scene)
+    return True
+  else:
+    error = "uknown preset"
+    return False
+  return True
+
+def make_dir(dirname):
+  if(fs.path_exists(dirname)): return
+  try:
+    fs.path_create_directories(dirname)
+  except:
+    cli.print_fatal("cannot create directory " + dirname)
+
+  
 
 
 def main(*argv):
@@ -22,4 +41,20 @@ def main(*argv):
   cli.add_option(cli, "scene", filename, "input scene", True)
 
   #load scene
-  
+  ext = fs.path_extension(filename)
+  basename = fs.path_stem(filename)
+  scene = ptr.scene()
+  ioerror = ""
+
+  if (ext == ".ypreset"):
+    cli.print_progress("make preset", 0, 1)
+    if not (make_preset(scene, basename, ioerror)):
+      cli.print_progress("make preset", 0, 1)
+  else:
+    if not (sio.load_scene(filename, scene , ioerror)):
+      cli.print_fatal(ioerror)
+
+
+  # if (copyrigt != ""):
+  #   for error in 
+
